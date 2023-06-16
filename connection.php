fadad2d4a286e52fe356a40605cec93b9d3d1818
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-06-16 15:13:48 +0630

    setup admin lte dashboard

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+
+// $pdo=new PDO("mysql:host=localhost;dbname=inventory_point_of_sales_project", "root", "Aung123580Zaw@");
+
+
+// echo "Connected.";
---
+++ footer.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+</footer>
+</div>
+<script src="plugins/jquery/jquery.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
