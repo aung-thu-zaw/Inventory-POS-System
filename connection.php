ce46aec532a57787be847dbed1a5dbab8c4493c5
aung-thu-zaw <aungthuzaw223@gmail.com>
2023-06-16 15:24:20 +0630

    connected with database

--- connection.php
+++ connection.php
@@ -1,6 +1,10 @@
 <?php
 
-// $pdo=new PDO("mysql:host=localhost;dbname=inventory_point_of_sales_project", "root", "Aung123580Zaw@");
+try {
+    $pdo=new PDO("mysql:host=localhost;dbname=inventory_point_of_sales_project", "root", "Aung123580Zaw@");
 
+    echo "Connected.";
+} catch(PDOException $error) {
 
-// echo "Connected.";
+    echo $error->getMessage();
+}
